1ac28ec5d93d5ba01dd03aaf699d8014c0ee2d9b
Shahbaz <mr.shahbux@gmail.com>
2026-07-18 20:31:00 +0530

    Implement section display and smooth scroll functionality
    Added function to show a specific section and hide others.

--- scripet.js
+++ scripet.js
@@ -1,1 +1,22 @@
 
+function showSection(section){
+
+    // sab sections hide karo
+    document.getElementById("memories").style.display = "none";
+    document.getElementById("dates").style.display = "none";
+    document.getElementById("voice").style.display = "none";
+    document.getElementById("sos").style.display = "none";
+
+
+    // jis button par click hua uska section show karo
+    let openSection = document.getElementById(section);
+
+    openSection.style.display = "block";
+
+
+    // smooth scroll
+    openSection.scrollIntoView({
+        behavior: "smooth"
+    });
+
+}
